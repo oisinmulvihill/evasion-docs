2044b3b623b55e018abb480ae1f51fe1d03abf87
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2009-10-16 17:27:14 +0100

    working on evasion project api docs

--- docrelease.sh
+++ docrelease.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-make html && rm -rf /opt/trackers/evasionproject/apidocs/* && cp -R build/html/* /opt/trackers/evasionproject/apidocs && cp /opt/trackers/evasionproject/apidocs/evasionproject.html  /opt/trackers/evasionproject/apidocs/index.html 
+make html && rm -rf /opt/trackers/evasionproject/apidocs/* && \
+  cp -R build/html/* /opt/trackers/evasionproject/apidocs && \
+  cp -R images /opt/trackers/evasionproject/apidocs && \
+  cp /opt/trackers/evasionproject/apidocs/evasionproject.html /opt/trackers/evasionproject/apidocs/index.html 
