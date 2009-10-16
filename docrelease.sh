923c00bc4b369bc587934887316f2a3912ebfaa5
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2009-10-16 16:34:11 +0100

    add docs release script to push out project api docs onto web served location

---
+++ docrelease.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+make html && rm -rf /opt/trackers/evasionproject/apidocs/* && cp -R build/html/* /opt/trackers/evasionproject/apidocs && cp /opt/trackers/evasionproject/apidocs/evasionproject.html  /opt/trackers/evasionproject/apidocs/index.html 
